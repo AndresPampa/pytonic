9159c05802045bae61469e7d70a3abb638f5337b
Pampa <andrespampa@gmail.com>
2026-08-20 21:50:17 -0300

    Terminando con enumerador con mas atributos - video 134

---
+++ poo/5-car_sample_dataclass.py
@@ -0,0 +1,45 @@
+from car import Car
+from datetime import datetime, timezone
+from constants import Constants
+from color import Color
+
+Car.set_license_plate_color(Constants.COLOR_RED)
+print(Car.get_license_plate_color())
+
+Car.set_license_plate_color(Color.RED.value)
+print(Car.get_license_plate_color())
+
+car1 = Car.full_spec(manufacturer='mazda',model='3', color=Color.YELLOW.value, cylinder=None, tank_capacity=34.00)
+Car.license_plate_color = 'Red' #se puede cambiar el atributo estatico de la clase desde la instancia, solo desde la clase
+print(car1)
+
+car2 = Car.full_spec(manufacturer='mazda',model='3', color=Car.COLOR_PURPLE, cylinder=None, tank_capacity=34.00)
+print(car2)
+
+car3 = car1
+date_now = datetime.now(tz=timezone.utc)
+print(date_now)
+
+print(car1 == car2) #== compara si son iguales, es decir, si tienen los mismos valores
+print(car1 is car2) #is compara si son el mismo objeto, es decir, si son la misma instancia
+print(car1 is car3) #TRUE
+print(car1 == date_now) #False
+
+#los atributos estaticos no le pertenecen a la instancia, sino a la clase, por lo que no se pueden comparar con el metodo __eq__
+
+car1.set_license_plate_color('Blue')
+print(car1.get_license_plate_color())
+
+car4 = Car.full_spec(manufacturer='mazda',model='4', color=Car.COLOR_WHITE, cylinder=None, tank_capacity=65.00)
+print(car4)
+
+speed_highway = Constants.MAX_SPEED_HIGHWAY
+print("speed_highway:",speed_highway)
+
+Constants.MAX_SPEED_HIGHWAY = 240 #ESTO NO SE DEBE HACER, YA QUE ES UNA CONSTANTE
+print("speed_highway:", Constants.MAX_SPEED_HIGHWAY)
+
+#Los enums son iterables
+for color in Color:
+    print(color)
+    print(color.value)
---
+++ poo/color.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+#Enum es un tipo de dato que representa un conjunto de constantes
+class Color(Enum):
+    RED: str = 'Red'
+    YELLOW: str = 'Yellow'
+    GREEN: str = 'Green'
+    BLUE: str = 'Blue'
+    PURPLE: str = 'Purple'
+    ORANGE: str = 'Orange'
+    PINK: str = 'Pink'
+    BROWN: str = 'Brown'
+    GRAY: str = 'Gray'
+    BLACK: str = 'Black'
+    WHITE: str = 'White'
